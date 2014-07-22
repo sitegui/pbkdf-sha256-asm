c5c7393d5761d0df1a61cb72d9dd3a01ccc59ddf
Guilherme Souza <sitegui@sitegui.com.br>
2014-07-21 22:30:48 -0300

    Test suite for SHA256

--- c/main.c
+++ c/main.c
@@ -1,19 +1,6 @@
-#include <stdio.h>
-#include "sha256.h"
-
-void checkHash(const byte *message, word length, const char digest[65]) {
-	char temp[65];
-	sha256(message, length, temp);
-	for (int i=0; i<64; i++) {
-		if (digest[i] != temp[i]) {
-			printf("Fail with length %lu\n", length);
-			return;
-		}
-	}
-	puts("OK!");
-}
+#include "test/sha256.h"
 
 int main() {
-    checkHash((byte*)"", 0, "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+    testSha256();
     return 0;
 }
---
+++ c/test/sha256.h
@@ -0,0 +1,6 @@
+#ifndef SHA256_H_INCLUDED
+#define SHA256_H_INCLUDED
+
+void testSha256();
+
+#endif // SHA256_H_INCLUDED
